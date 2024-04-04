ecf9879bb2a8821b08a3eb0ca160a1e782606132
Aishwarya-koli <124526805+AishwaryaKoli@users.noreply.github.com>
2024-04-04 10:05:10 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,83 +1,28 @@
-from flask import Flask, request, jsonify,render_template
-from flask_mysqldb import MySQL
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-# Configure MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'ghibli'
-app.config['MYSQL_DB'] = 'art_shows'
-mysql = MySQL(app)
-
-# API endpoints
+# Sample art shows data
+art_shows = [
+    {
+        'title': 'ART EXHIBITION',
+        'description': 'A visual object experience consciously created through an expression of skill or imagination. ',
+        'image': 'art-gallery-exhibition-vertical-poster-template_23-2149721708.jpg'
+    },
+    {
+        'title': 'THE ART FAIR',
+        'description': 'It is the subcontinent\'s most-awaited fair... a treat for collectors and connoisseurs.',
+        'image': 'images.jpeg'
+    }
+]
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/get_art_shows')
+def get_art_shows():
+    return jsonify({'artShows': art_shows})
 
-@app.route('/create_show', methods=['POST'])
-def create_show():
-    # Establish MySQL connection within the request handler function
-    cur = mysql.connection.cursor()
-    
-    data = request.json
-    title = data['title']
-    date = data['date']
-    venue = data['venue']
-    tickets_available = data['tickets_available']
-
-    cur.execute("INSERT INTO art_show (title, date, venue, tickets_available) VALUES (%s, %s, %s, %s)",
-                (title, date, venue, tickets_available))
-    mysql.connection.commit()
-    cur.close()
-
-    return jsonify({'message': 'Art show created successfully'}), 201
-
-@app.route('/book_tickets', methods=['POST'])
-def book_tickets():
-    # Establish MySQL connection within the request handler function
-    cur = mysql.connection.cursor()
-    
-    data = request.json
-    show_id = data['show_id']
-    user_name = data['user_name']
-    tickets_booked = data['tickets_booked']
-
-    cur.execute("SELECT tickets_available FROM art_show WHERE id = %s", (show_id,))
-    show = cur.fetchone()
-    if show and show[0] >= tickets_booked:
-        cur.execute("UPDATE art_show SET tickets_available = tickets_available - %s WHERE id = %s",
-                    (tickets_booked, show_id))
-        cur.execute("INSERT INTO booking (show_id, user_name, tickets_booked) VALUES (%s, %s, %s)",
-                    (show_id, user_name, tickets_booked))
-        mysql.connection.commit()
-        cur.close()
-        return jsonify({'message': 'Tickets booked successfully'}), 201
-    else:
-        cur.close()
-        return jsonify({'error': 'Not enough tickets available'}), 400
-
-@app.route('/shows', methods=['GET'])
-def get_shows():
-    # Establish MySQL connection within the request handler function
-    cur = mysql.connection.cursor()
-    
-    cur.execute("SELECT * FROM art_show")
-    shows = cur.fetchall()
-    cur.close()
-    return jsonify({'shows': shows})
-
-@app.route('/bookings', methods=['GET'])
-def get_bookings():
-    # Establish MySQL connection within the request handler function
-    cur = mysql.connection.cursor()
-    
-    cur.execute("SELECT * FROM booking")
-    bookings = cur.fetchall()
-    cur.close()
-    return jsonify({'bookings': bookings})
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
